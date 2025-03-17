6e63fadea5f040aa2fceb5a7794e484ee1628f67
wliustc <wliustc@163.com>
2025-03-17 13:37:10 +0800

    Update loadtorrentparams.h
     bool firstLastPiecePriority = true;

--- src/base/bittorrent/loadtorrentparams.h
+++ src/base/bittorrent/loadtorrentparams.h
@@ -53,7 +53,7 @@ namespace BitTorrent
         TorrentContentLayout contentLayout = TorrentContentLayout::Original;
         TorrentOperatingMode operatingMode = TorrentOperatingMode::AutoManaged;
         bool useAutoTMM = false;
-        bool firstLastPiecePriority = false;
+        bool firstLastPiecePriority = true;
         bool hasFinishedStatus = false;
         bool stopped = false;
         Torrent::StopCondition stopCondition = Torrent::StopCondition::None;
